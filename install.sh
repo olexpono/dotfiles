52efdfae70e01973e5f32d4cb6d9c2df8a8fa0bc
Olex Ponomarenko <olex.ponomarenko@vanta.com>
2025-09-24 12:24:25 -0400

    Add example dotfiles, chsh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
